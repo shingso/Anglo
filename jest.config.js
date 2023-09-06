df62fa3254b06402b642854ca6c922440146783b
Shing So <shingso026@gmail.com>
2023-09-06 11:59:23 -0400

    Fix jest configs

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,11 @@ module.exports = {
     "<rootDir>/node_modules/(react-clone-referenced-element|@react-native-community|react-navigation|@react-navigation/.*|@unimodules/.*|native-base|react-native-code-push)",
     "jest-runner",
   ],
+  transform: {
+    "^.+\\.[jt]sx?$": "babel-jest",
+  },
   testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.maestro/", "@react-native"],
-  testEnvironment: "jsdom",
+  testEnvironment: "node",
   setupFiles: ["<rootDir>/test/setup.ts"],
+  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
 }
