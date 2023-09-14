6c560bc7582e31a19f5106f10382d0b430344712
Shing So <shingso026@gmail.com>
2023-09-14 10:19:31 -0400

    Continue tutorial screen

--- app/screens/TutorialScreen.tsx
+++ app/screens/TutorialScreen.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useEffect } from "react"
 import { observer } from "mobx-react-lite"
-import { View, ViewStyle } from "react-native"
+import { View, ViewStyle, Image } from "react-native"
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import { AppStackScreenProps } from "app/navigators"
 import { Button, CustomText, Icon, Screen, Text } from "app/components"
@@ -9,7 +9,7 @@ import { Button, CustomText, Icon, Screen, Text } from "app/components"
 import AppIntroSlider from "react-native-app-intro-slider"
 import { saveTutorialSeen } from "app/utils/storage/tutorialUtils"
 import { getAllKeys } from "app/utils/storage"
-import { spacing } from "app/theme"
+import { custom_palette, spacing } from "app/theme"
 
 interface TutorialScreenProps extends NativeStackScreenProps<AppStackScreenProps<"Tutorial">> {}
 
@@ -26,46 +26,56 @@ export const TutorialScreen: FC<TutorialScreenProps> = observer(function Tutoria
   const slides = [
     {
       key: "one",
-      title: "Title 1",
-      text: "Spaced Repition",
-
-      backgroundColor: "#59b2ab",
+      title: "Study less, know it better",
+      text: "Studying is hard. Studying efficiently is even harder. See how this app helps you retain information better.",
+      image: require("../../assets/images/girl_looking_at_phone.png"),
     },
     {
       key: "two",
-      title: "Title 2",
-      text: "You review information at longer intervals over time",
-
+      title: "Our memory spans",
+      text: "When we try to memorize something, our brains will forget the information over a period of time",
+      image: require("../../assets/images/goldfish_in_bowl.png"),
       backgroundColor: "#febe29",
     },
     {
       key: "three",
-      title: "lorum ipsum",
-      text: "It helps your brain remember things better by spacing out practice.",
+      title: "Spaced repitition",
+      image: require("../../assets/images/girl_looking_at_phone.png"),
+      text: "By revisting what we are trying to memorize before the we completely forget it, we can extend our memory retention",
       backgroundColor: "#22bcb5",
     },
     {
       key: "four",
-      title: "lorum ipsum",
-      text: "When studing swipe right if you know the card.",
+      image: require("../../assets/images/girl_looking_at_phone.png"),
+      title: "Smart cards",
+      text: "By using Anglo flashcards, the app can figure out the best time to show you the card again.",
       backgroundColor: "#22bcb5",
     },
     {
       key: "five",
-      title: "lorum ipsum",
-      text: "Swipe left if you dont know at all",
+      image: require("../../assets/images/girl_looking_at_phone.png"),
+      title: "You know it!",
+      text: "If you know the definition of the card confidently, swipe the card left",
       backgroundColor: "#22bcb5",
     },
     {
       key: "six",
-      title: "lorum ipsum",
-      text: "Or swipe up if you somewhat can remember the card.",
+      image: require("../../assets/images/girl_looking_at_phone.png"),
+      title: "You know it, but weren't confident about it",
+      text: "If you got the definition correct, were unsure and took some time recalling, swipe the card up.",
       backgroundColor: "#22bcb5",
     },
     {
       key: "seven",
-      title: "lorum ipsum",
-      text: "AI will figure out the best interval to show the card again.",
+      image: require("../../assets/images/girl_looking_at_phone.png"),
+      title: "Forgot card",
+      text: "If you don't know the defintition, swipe left",
+    },
+    {
+      key: "eight",
+      image: require("../../assets/images/girl_looking_at_phone.png"),
+      title: "Review cards at intervals",
+      text: "As you know the card better and better, it the interval will continue to grow. Consistently come back and study the cards",
       backgroundColor: "#22bcb5",
     },
   ]
@@ -75,20 +85,31 @@ export const TutorialScreen: FC<TutorialScreenProps> = observer(function Tutoria
       <View
         style={{
           flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
           padding: spacing.size200,
+          paddingBottom: spacing.size400,
         }}
       >
-        <CustomText preset="title3">{item.title}</CustomText>
+        <Image
+          style={{ height: 275, width: 275, marginBottom: spacing.size200 }}
+          source={item.image}
+        />
+        <CustomText style={{ textAlign: "center", marginBottom: spacing.size80 }} preset="title2">
+          {item.title}
+        </CustomText>
 
-        <CustomText preset="body1">{item.text}</CustomText>
+        <CustomText style={{ textAlign: "center" }} preset="body1">
+          {item.text}
+        </CustomText>
       </View>
     )
   }
 
   const _renderNextButton = () => {
     return (
-      <View>
-        <Icon icon="search" size={24} />
+      <View style={{}}>
+        <CustomText preset="body1Strong">Next</CustomText>
       </View>
     )
   }
@@ -97,7 +118,14 @@ export const TutorialScreen: FC<TutorialScreenProps> = observer(function Tutoria
   // const navigation = useNavigation()
   return (
     <View style={$root}>
-      <AppIntroSlider renderItem={_renderItem} data={slides} onDone={_onDone} />
+      <AppIntroSlider
+        activeDotStyle={{ backgroundColor: custom_palette.primary100 }}
+        dotStyle={{ backgroundColor: custom_palette.primary60 }}
+        renderNextButton={_renderNextButton}
+        renderItem={_renderItem}
+        data={slides}
+        onDone={_onDone}
+      />
     </View>
   )
 })
