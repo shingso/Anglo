57163613a22955328542cf42462e6bd70f1fdf1b
Shing So <shingso026@gmail.com>
2023-12-21 17:43:26 -0800

    Add social sign in to sign up screen

--- app/screens/SignUpScreen.tsx
+++ app/screens/SignUpScreen.tsx
@@ -1,19 +1,24 @@
 import React, { FC, useMemo, useState } from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle } from "react-native"
+import { View, ViewStyle } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { AppStackScreenProps } from "../navigators"
 import {
   Button,
   CustomText,
+  Header,
   Icon,
+  LineWord,
   Screen,
   Text,
   TextField,
   TextFieldAccessoryProps,
 } from "../components"
-import { custom_colors, spacing } from "../theme"
+import { custom_colors, spacing, typography } from "../theme"
 import { supabase } from "../services/supabase/supabase"
+import { makeRedirectUri } from "expo-auth-session"
+import { AuthProviders } from "./LoginScreen"
+import * as WebBrowser from "expo-web-browser"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../models"
 
@@ -56,31 +61,108 @@ export const SignUpScreen: FC<StackScreenProps<AppStackScreenProps, "SignUp">> =
       [isPasswordHidden],
     )
 
-    return (
-      <Screen safeAreaEdges={["top"]} style={$root} preset="scroll">
-        <CustomText style={{ marginBottom: spacing.size120 }} preset="body1Strong">
-          Sign Up
-        </CustomText>
+    const signInWithSocialAuth = async (provider: AuthProviders) => {
+      const extractParamsFromUrl = (url: string) => {
+        const params = new URLSearchParams(url.split("#")[1])
+        const data = {
+          access_token: params.get("access_token"),
+          expires_in: parseInt(params.get("expires_in") || "0"),
+          refresh_token: params.get("refresh_token"),
+          token_type: params.get("token_type"),
+          provider_token: params.get("provider_token"),
+        }
+        return data
+      }
+      const redirectUrl = makeRedirectUri()
+      const oAuthResult = await supabase.auth.signInWithOAuth({
+        provider: provider,
+        options: {
+          redirectTo: redirectUrl,
+        },
+      })
 
-        <TextField
-          value={email}
-          placeholder="Email"
-          autoCapitalize="none"
-          onChangeText={setEmail}
-          containerStyle={$inputContainer}
-        ></TextField>
-        <TextField
-          value={password}
-          autoCapitalize="none"
-          placeholder="Password"
-          onChangeText={setPassword}
-          secureTextEntry={isPasswordHidden}
-          containerStyle={$inputContainer}
-          RightAccessory={PasswordRightAccessory}
-        ></TextField>
-        <Button preset="custom_default" onPress={() => signUp(email, password)}>
-          Sign Up
-        </Button>
+      if (!oAuthResult) return
+      const signInResult = await WebBrowser.openAuthSessionAsync(
+        oAuthResult?.data?.url,
+        redirectUrl,
+      )
+
+      if (signInResult?.type === "success") {
+        const data = extractParamsFromUrl(signInResult.url)
+        supabase.auth.setSession({
+          access_token: data?.access_token,
+          refresh_token: data?.refresh_token,
+        })
+      }
+    }
+
+    return (
+      <Screen style={$root} preset="scroll">
+        <Header title={"Sign up"}></Header>
+
+        <View style={$container}>
+          <View style={{ width: 300, marginBottom: spacing.size320 }}>
+            <CustomText
+              preset="title1"
+              style={{ marginBottom: spacing.size40, fontFamily: typography.primary.semiBold }}
+            >
+              Get more out of your studying
+            </CustomText>
+          </View>
+
+          <TextField
+            value={email}
+            placeholder="Email"
+            autoCapitalize="none"
+            onChangeText={setEmail}
+            containerStyle={$inputContainer}
+          ></TextField>
+          <TextField
+            value={password}
+            autoCapitalize="none"
+            placeholder="Password"
+            onChangeText={setPassword}
+            secureTextEntry={isPasswordHidden}
+            containerStyle={$inputContainer}
+            RightAccessory={PasswordRightAccessory}
+          ></TextField>
+
+          <Button preset="custom_default" onPress={() => signUp(email, password)}>
+            Sign Up
+          </Button>
+          <LineWord text="or"></LineWord>
+          <Button
+            preset="custom_outline"
+            style={{
+              marginBottom: spacing.size160,
+              height: 44,
+              borderColor: custom_colors.foreground3,
+            }}
+            textStyle={{ fontSize: 16, lineHeight: 26, color: custom_colors.foreground3 }}
+            LeftAccessory={(props) => (
+              <Icon color={null} containerStyle={props.style} icon="google_logo" size={22}></Icon>
+            )}
+            onPress={() => signInWithSocialAuth(AuthProviders.GOOGLE)}
+          >
+            Continue with Google
+          </Button>
+          <Button
+            preset="custom_outline"
+            style={{
+              marginBottom: spacing.size160,
+              height: 44,
+              backgroundColor: "black",
+              borderWidth: 0,
+            }}
+            textStyle={{ fontSize: 16, lineHeight: 26, color: "white" }}
+            LeftAccessory={(props) => (
+              <Icon containerStyle={props.style} icon="apple_logo" color="white" size={22}></Icon>
+            )}
+            onPress={() => signInWithSocialAuth(AuthProviders.APPLE)}
+          >
+            Continue with Apple
+          </Button>
+        </View>
       </Screen>
     )
   },
@@ -88,7 +170,10 @@ export const SignUpScreen: FC<StackScreenProps<AppStackScreenProps, "SignUp">> =
 
 const $root: ViewStyle = {
   flex: 1,
-  padding: spacing.large,
+}
+
+const $container: ViewStyle = {
+  padding: spacing.size160,
 }
 
 const $inputContainer: ViewStyle = {
